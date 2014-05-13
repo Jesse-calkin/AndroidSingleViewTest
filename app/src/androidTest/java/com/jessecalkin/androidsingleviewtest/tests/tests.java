db5f819f3b4300ae7f9228f04aad1853a67bb9bd
jesse-calkin <jesse.calkin@gmail.com>
2014-05-13 10:18:58 -0600

    initial commit, no gradle warnings

---
+++ app/src/androidTest/java/com/jessecalkin/androidsingleviewtest/tests/tests.java
@@ -0,0 +1,14 @@
+package com.jessecalkin.androidsingleviewtest.tests;
+import android.test.ActivityInstrumentationTestCase2;
+import com.singleviewtest.app.FullscreenActivity;
+
+/**
+ * Created by jesse on 5/12/14.
+ */
+public class tests extends ActivityInstrumentationTestCase2 <FullscreenActivity> {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+    }
+}
