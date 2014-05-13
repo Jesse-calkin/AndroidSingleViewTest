c8424db9dafef9d64aafcb07e51233b179188c96
jesse-calkin <jesse.calkin@gmail.com>
2014-05-13 11:00:42 -0600

    kinda working, compiles but some kind of signing permission error...I give up for today

--- app/src/androidTest/java/com/jessecalkin/androidsingleviewtest/tests/tests.java
+++ app/src/androidTest/java/com/jessecalkin/androidsingleviewtest/tests/tests.java
@@ -5,10 +5,18 @@ import com.singleviewtest.app.FullscreenActivity;
 /**
  * Created by jesse on 5/12/14.
  */
-public class tests extends ActivityInstrumentationTestCase2 <FullscreenActivity> {
+public class tests extends ActivityInstrumentationTestCase2<FullscreenActivity>  {
+    private FullscreenActivity mFullscreenActivity;
+
+    @SuppressWarnings("deprecation")
+    public tests() {
+        super("com.singleviewtest.app", FullscreenActivity.class);
+    }
 
     protected void setUp() throws Exception {
         super.setUp();
+
+        mFullscreenActivity = getActivity();
 
     }
 }
